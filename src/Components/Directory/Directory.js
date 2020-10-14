d06b7f56b54376d77de49898c58d036fa8d5809b
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-14 14:19:50 -0400

    Added size styles for larger pictures

--- src/Components/Directory/Directory.js
+++ src/Components/Directory/Directory.js
@@ -48,8 +48,12 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, id, imageURL}) => {
-                        return <MenuItem key={id} title={title} />
+                    this.state.sections.map(({title, id, imageUrl, size}) => {
+                        return <MenuItem 
+                            key={id} 
+                            title={title} 
+                            imageUrl={imageUrl}
+                            size={size}/>
                     })
                 }
             </div>
