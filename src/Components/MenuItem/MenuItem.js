43d24804adbb39f2c3a96787cce85aff8d0c6806
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-14 14:23:43 -0400

    Added size styling of images to make them larger for different images

--- src/Components/MenuItem/MenuItem.js
+++ src/Components/MenuItem/MenuItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './MenuItem.scss';
 
-const MenuItem = ({title}) => {
-    return <div className='menu-item'>
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size }) => {
+    return (
+        <div 
+            style={{backgroundImage: `url(${imageUrl})`}}
+            className={`menu-item ${size}`}>
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default MenuItem;
