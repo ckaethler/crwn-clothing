898f32942c98077c3f811ee1c0ad80f153988705
Catherine Kaethler <catherinekaethler@Catherines-MacBook-Pro.local>
2020-10-16 15:20:41 -0400

    updating our app with a shop page, collection preview component, collection component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import HomePage from './Pages/homepage/Homepage';
 import { Route } from 'react-router-dom';
+import HomePage from './Pages/homepage/Homepage';
+import ShopPage from './Pages/shop/ShopPage';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
